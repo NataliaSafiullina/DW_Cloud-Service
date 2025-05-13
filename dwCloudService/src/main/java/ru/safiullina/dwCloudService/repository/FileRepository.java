81cd2b88444072bbfd42c6b9fa8c1f2e783fa6f1
NataliaSafiullina <nfsafiullina@gmail.com>
2025-05-13 22:13:38 +0700

    Add repositiry methods

--- dwCloudService/src/main/java/ru/safiullina/dwCloudService/repository/FileRepository.java
+++ dwCloudService/src/main/java/ru/safiullina/dwCloudService/repository/FileRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.safiullina.dwCloudService.entity.File;
 
+import java.util.Optional;
+
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    Optional<File> findByFileName(String fileName);
+
 }
--- dwCloudService/src/main/java/ru/safiullina/dwCloudService/repository/UserRepository.java
+++ dwCloudService/src/main/java/ru/safiullina/dwCloudService/repository/UserRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.safiullina.dwCloudService.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByLogin(String login);
+    Boolean existsByLogin(String login);
+
 }
